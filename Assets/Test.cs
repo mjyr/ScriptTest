c382f29b4d5a500739d45eda620acca63df32ff7
mjyr <mjyr0114@gmail.com>
2019-04-18 12:14:52 +0900

    ScriptTestプロジェクト　Lesson4課題提出
    配列宣言をして出力したファイルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // 要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化してください
+        int[] array = {10,20,30,40,50};
+
+        // for文を使い、配列の各要素の値を順番に表示してください
+        for (int i = 0; i< 5; i++) {
+            Debug.Log(array[i]);
+        }
+
+        // for文を使い、配列の各要素の値を逆順に表示してください
+        for (int i = array.Length - 1; 0<= i; i--) {
+            Debug.Log(array[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
